54a16a7f15cd03f95e7aa1c7e8a410f00ef01ad5
MRloll <w.abdelkrim@wakeb.tech>
2026-05-28 20:19:59 +0300

    add module fedration and expose reactApp

--- apps/vue/src/views/index.vue
+++ apps/vue/src/views/index.vue
@@ -1,21 +1,20 @@
 <script setup lang="ts">
+  import { defineAsyncComponent } from "vue";
   import type { user } from "@repo/types";
-
   const user: user = {
     email: "test",
     id: 1,
     name: "test",
     password: "122",
   };
+
+  const ReactApp = await defineAsyncComponent(
+    () => import("react_app/ReactApp"),
+  );
 </script>
 
 <template>
   <div className="bg-green-400">
-    <h1>
-      {{ user.name }}
-    </h1>
-    <p>
-      {{ user.email }}
-    </p>
+    <ReactApp />
   </div>
 </template>
